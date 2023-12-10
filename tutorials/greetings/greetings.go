4b268800db5407bf302a15430f43206a6c391b96
Daniel Wait <waitdaniel4@gmail.com>
2023-12-10 19:17:24 +0200

    Error handling

--- tutorials/greetings/greetings.go
+++ tutorials/greetings/greetings.go
@@ -1,16 +1,22 @@
 package greetings
 
+import "errors"
 import "fmt"
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string,error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
     // Return a greeting that embeds the name in a message.
 	if false {
 		message := fmt.Sprintf("Hi, %v. Welcome!", name)
-		return message
+		return message, nil
 	} else {
 		var message string
 		message = fmt.Sprintf("Hi, %v. Welcome!", name)
-		return message
+		return message, nil
 	}
 }
